22e2a1f691eae0aa2750a7aa8b420cc9d9c04e95
Manny Pissanos <mpissano@gmail.com>
2018-09-10 12:53:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,6 @@ end
 
 def take_a_number(array, name)
   array.push(name)
-  puts "Welcome #{name}, You are number #{array.to_i + 1} in line."
+  puts "Welcome #{name}, You are number #{array.number} in line."
  end
  
